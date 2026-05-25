dabb66e4d9b50dc234587d8c8cfa60a13b7350eb
Comson <laker6679@gmail.com>
2026-05-25 09:56:49 +0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,18 +1,1 @@
--- Loader for FrendlyHub
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
--- HWID Lock
-local allowedUsers = {
-    [10795177721] = true,
-    [7508375923] = true,
-    [11000050138] = true,
-}
-
-if not allowedUsers[LocalPlayer.UserId] then
-    LocalPlayer:Kick("❌ Access Denied")
-    while true do wait(9e9) end
-end
-
--- Load main script from Pastebin (hidden)
-loadstring(game:HttpGet("https://pastebin.com/raw/uSJYKxHa"))()
