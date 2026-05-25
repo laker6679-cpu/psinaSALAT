21ccf6ffab90bfc32580429f52e3797988a6226b
Comson <laker6679@gmail.com>
2026-05-25 10:16:21 +0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,15 +1,15 @@
--- Публичный загрузчик FrendlyHub
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
--- Твои ID (кого пускаем)
+
 local AllowedUsers = {
     10795177721,
     7508375923,
     11000050138,
 }
 
--- Проверка доступа
+
 local allowed = false
 for _, id in pairs(AllowedUsers) do
     if LocalPlayer.UserId == id then
@@ -23,10 +23,9 @@ if not allowed then
     while true do task.wait(9e9) end
 end
 
--- Ссылка на секретный скрипт (ЗАМЕНИ НА СВОЮ НОВУЮ ССЫЛКУ)
+
 local scriptUrl = "https://pastebin.com/raw/DghCqjr6"
 
--- Загрузка и запуск
 local success, err = pcall(function()
     loadstring(game:HttpGet(scriptUrl))()
 end)
